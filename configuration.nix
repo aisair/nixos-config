86614d75192b31f3fe9c85ddeb02f162183640c6
aisair <ais11air@gmail.com>
2026-06-27 02:31:34 -0500

    feat: enable ssh agent

--- configuration.nix
+++ configuration.nix
@@ -44,6 +44,8 @@
   # Set your time zone.
   time.timeZone = "America/Chicago";
 
+  programs.ssh.startAgent = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
