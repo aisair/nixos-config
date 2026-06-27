d4fce5a0e877dd544142441c83d18dcb12b9db00
aisair <ais11air@gmail.com>
2026-06-27 17:14:14 -0500

    feat: Add GnuPG functionality

--- configuration.nix
+++ configuration.nix
@@ -127,8 +127,11 @@
     helix
     ghostty.terminfo
     git
-    gnupg
   ];
+
+  programs.gnupg.agent = {
+    enable = true;
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
